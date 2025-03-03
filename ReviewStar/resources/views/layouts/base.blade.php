3b91e004782d7e095b9ef77fe4cd521ca4df142a
Troyi <atrovin365@iesfuengirola1.es>
2025-03-03 17:56:18 +0100

    arreglo final css login + register

--- ReviewStar/resources/views/layouts/base.blade.php
+++ ReviewStar/resources/views/layouts/base.blade.php
@@ -10,15 +10,15 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-        crossorigin="anonymous"></script>
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
 
     <!-- Link css -->
     <link href="{{ asset('css/estilos.css') }}" rel="stylesheet">
 
     <!-- Link iconos -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-    
+
     <!-- Scripts -->
     <script src="js/jquery-3.7.1.min.js" defer></script>
 
@@ -32,7 +32,7 @@
             <div class="container-fluid">
                 <!-- Logo -->
                 <a class="navbar-brand" href="{{ route('base') }}">
-                    <img src="{{asset('assets/img/logo.png')}}" alt="Logo" height="50">
+                    <img src="{{ asset('assets/img/logo.png') }}" alt="Logo" height="50">
                 </a>
 
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
@@ -45,31 +45,36 @@
                         <li class="nav-item">
                             <a class="nav-link {{ Route::currentRouteName() == 'base' ? 'active' : '' }}"
                                 href="{{ route('base') }}">
-                                <img src="{{asset('assets/iconos/home.svg')}}" class="nav-icon" alt="Inicio"> Inicio
+                                <img src="{{ asset('assets/iconos/home.svg') }}" class="nav-icon" alt="Inicio"> Inicio
                             </a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link {{ Route::currentRouteName() == 'conciertos' ? 'active' : '' }}"
                                 href="{{ route('conciertos') }}">
-                                <img src="{{asset('assets/iconos/ticket.svg')}}" class="nav-icon" alt="Conciertos">
+                                <img src="{{ asset('assets/iconos/ticket.svg') }}" class="nav-icon" alt="Conciertos">
                                 Conciertos
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link {{ Route::currentRouteName() == 'favoritos' ? 'active' : '' }}" href="">
-                                <img src="{{asset('assets/iconos/favoritos.svg')}}" class="nav-icon" alt="Favoritos">
+                            <a class="nav-link {{ Route::currentRouteName() == 'favoritos' ? 'active' : '' }}"
+                                href="">
+                                <img src="{{ asset('assets/iconos/favoritos.svg') }}" class="nav-icon" alt="Favoritos">
                                 Favoritos
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link {{ Route::currentRouteName() == 'sobre' ? 'active' : '' }}" href="">
-                                <img src="{{asset('assets/iconos/informacion.svg')}}" class="nav-icon" alt="Sobre Nosotros">
+                            <a class="nav-link {{ Route::currentRouteName() == 'sobre' ? 'active' : '' }}"
+                                href="">
+                                <img src="{{ asset('assets/iconos/informacion.svg') }}" class="nav-icon"
+                                    alt="Sobre Nosotros">
                                 Sobre Nosotros
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link {{ Route::currentRouteName() == 'contacto' ? 'active' : '' }}" href="">
-                                <img src="{{asset('assets/iconos/contacto.svg')}}" class="nav-icon" alt="Contacto"> Contacto
+                            <a class="nav-link {{ Route::currentRouteName() == 'contacto' ? 'active' : '' }}"
+                                href="">
+                                <img src="{{ asset('assets/iconos/contacto.svg') }}" class="nav-icon" alt="Contacto">
+                                Contacto
 
                             </a>
                         </li>
@@ -97,7 +102,8 @@
                                         <li>
                                             <hr class="dropdown-divider">
                                         </li>
-                                        <li><a class="dropdown-item" href="{{route('logout')}}">Cerrar sesión</a></li>
+                                        <li><a class="dropdown-item" href="{{ route('logout') }}">Cerrar sesión</a>
+                                        </li>
                                     </ul>
                                 </div>
                             </div>
@@ -119,7 +125,7 @@
     </footer>
 
     <script>
-        document.addEventListener('DOMContentLoaded', function () {
+        document.addEventListener('DOMContentLoaded', function() {
             // Obtener la ruta actual
             const currentRoute = "{{ Route::currentRouteName() }}";
 
