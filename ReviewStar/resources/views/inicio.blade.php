ed1b767a95825f367eb2dff1553fc92ac4aaf4f6
Jonay Aguilar Menéndez <joonaayag19@gmail.com>
2025-03-04 22:07:40 +0100

    Funcionalidad de página de inicio hecha, junto a las peticiones ajax a falta de estilos

--- ReviewStar/resources/views/inicio.blade.php
+++ ReviewStar/resources/views/inicio.blade.php
@@ -10,139 +10,16 @@
     <div class="container mt-5">
         <h2>¿Qúe te apetece hacer hoy?</h2>
         <div class="row">
-            <div class="col-md-6"style="background-color: pink;">
-                <div class="row mb-3">
-                    <div class="col-4">
-                        <img src="" alt="">
-                    </div>
-                    <div class="col-8">
-                        <h4>Nombre del evento</h4>
-                        <p>
-                        <p>Lugar: </p>
-                        <p>Fecha: </p>
-                        <p>Entradas: </p>
-                        </p>
-                        <a href="#" class="btn btn-primary">Más Detalles</a>
-                    </div>
-                </div>
-                <div class="row mb-3">
-                    <div class="col-4">
-                        <img src="" alt="">
-                    </div>
-                    <div class="col-8">
-                        <h4>Nombre del evento</h4>
-                        <p>
-                        <p>Lugar: </p>
-                        <p>Fecha: </p>
-                        <p>Entradas: </p>
-                        </p>
-                        <a href="#" class="btn btn-primary">Más Detalles</a>
-                    </div>
-                </div>
-                <div class="row mb-3">
-                    <div class="col-4">
-                        <img src="" alt="">
-                    </div>
-                    <div class="col-8">
-                        <h4>Nombre del evento</h4>
-                        <p>
-                        <p>Lugar: </p>
-                        <p>Fecha: </p>
-                        <p>Entradas: </p>
-                        </p>
-                        <a href="#" class="btn btn-primary">Más Detalles</a>
-                    </div>
-                </div>
-                <div class="row mb-3">
-                    <div class="col-4">
-                        <img src="" alt="">
-                    </div>
-                    <div class="col-8">
-                        <h4>Nombre del evento</h4>
-                        <p>
-                        <p>Lugar: </p>
-                        <p>Fecha: </p>
-                        <p>Entradas: </p>
-                        </p>
-                        <a href="#" class="btn btn-primary">Más Detalles</a>
-                    </div>
-                </div>
-                <div class="row mb-3">
-                    <div class="col-4">
-                        <img src="" alt="">
-                    </div>
-                    <div class="col-8">
-                        <h4>Nombre del evento</h4>
-                        <p>
-                        <p>Lugar: </p>
-                        <p>Fecha: </p>
-                        <p>Entradas: </p>
-                        </p>
-                        <a href="#" class="btn btn-primary">Más Detalles</a>
-                    </div>
-                </div>
+            <div class="col-md-6" id="columnaDeportes">
+                
             </div>
             <div class="col-md-6">
                 <div class="row" style="background-color: red;">
-                    <div class="col-12 mb-3">
-                        <div class="row mb-3">
-                            <div class="col-4">
-                                <img src="" alt="">
-                            </div>
-                            <div class="col-8">
-                                <h4>Nombre del evento</h4>
-                                <p>
-                                <p>Lugar: </p>
-                                <p>Fecha: </p>
-                                <p>Entradas: </p>
-                                </p>
-                                <a href="#" class="btn btn-primary">Más Detalles</a>
-                            </div>
-                        </div>
-                        <div class="row mb-3">
-                            <div class="col-4">
-                                <img src="" alt="">
-                            </div>
-                            <div class="col-8">
-                                <h4>Nombre del evento</h4>
-                                <p>
-                                <p>Lugar: </p>
-                                <p>Fecha: </p>
-                                <p>Entradas: </p>
-                                </p>
-                                <a href="#" class="btn btn-primary">Más Detalles</a>
-                            </div>
-                        </div>
+                    <div class="col-12 mb-3" id="columnaMusica">
+                        
                     </div>
-                    <div class="col-12">
-                        <div class="row mb-3">
-                            <div class="col-4">
-                                <img src="" alt="">
-                            </div>
-                            <div class="col-8">
-                                <h4>Nombre del evento</h4>
-                                <p>
-                                <p>Lugar: </p>
-                                <p>Fecha: </p>
-                                <p>Entradas: </p>
-                                </p>
-                                <a href="#" class="btn btn-primary">Más Detalles</a>
-                            </div>
-                        </div>
-                        <div class="row mb-3">
-                            <div class="col-4">
-                                <img src="" alt="">
-                            </div>
-                            <div class="col-8">
-                                <h4>Nombre del evento</h4>
-                                <p>
-                                <p>Lugar: </p>
-                                <p>Fecha: </p>
-                                <p>Entradas: </p>
-                                </p>
-                                <a href="#" class="btn btn-primary">Más Detalles</a>
-                            </div>
-                        </div>
+                    <div class="col-12" id="columnaArte">
+                        
                     </div>
                 </div>
             </div>
