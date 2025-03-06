76454541021b8967dfd3f27ecbecf9806206295a
Troyi <atrovin365@iesfuengirola1.es>
2025-03-06 21:22:53 +0100

    cambio providers para que se haga el link automático

--- ReviewStar/app/Providers/AppServiceProvider.php
+++ ReviewStar/app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +22,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (!Storage::exists('public')) {
+            Artisan::call('storage:link');
+        }
     }
 }
