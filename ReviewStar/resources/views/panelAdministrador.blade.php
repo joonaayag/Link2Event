bd251d80a8226c3d48d67539d95fc0f0bbe3bc10
Troyi <atrovin365@iesfuengirola1.es>
2025-03-07 14:02:23 +0100

    add ventanas modales en panel administrador

--- ReviewStar/resources/views/panelAdministrador.blade.php
+++ ReviewStar/resources/views/panelAdministrador.blade.php
@@ -14,7 +14,6 @@
                 {{ session('success') }}
                 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
             </div>
-        
         @endif
 
         <h2>Lista de Usuarios</h2>
@@ -36,25 +35,47 @@
                         <td>{{ $usuario->email }}</td>
                         <td>
                             <!-- Eliminar Usuario -->
-                            <form action="{{ route('eliminarUsuario') }}" method="post">
-                                @csrf
-                                @method('DELETE')
-                                <input type="hidden" name="id_usuario" value="{{ $usuario->id }}">
-                                <button type="submit" class="btn btn-danger">Eliminar usuario</button>
-                            </form>
+                            <button type="button" class="btn btn-primary btn-modal" data-bs-toggle="modal" data-bs-target="#modal_{{ $usuario->id }}">
+                                Eliminar usuario
+                            </button>
+
+                            <!-- Modal -->
+                            <div class="modal fade" id="modal_{{ $usuario->id }}" tabindex="-1" role="dialog"
+                                aria-labelledby="modalTitle_{{ $usuario->id }}" aria-hidden="true">
+                                <div class="modal-dialog modal-dialog-centered" role="document">
+                                    <div class="modal-content card">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title">¿Eliminar usuario?</h5>
+                                        </div>
+                                        <div class="modal-body">
+                                            <p>¿Seguro que quieres eliminar al usuario {{ $usuario->nombre }} {{ $usuario->apellidos }}?</p>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                                            <form action="{{ route('eliminarUsuario') }}" method="post" style="display:inline;">
+                                                @csrf
+                                                @method('DELETE')
+                                                <input type="hidden" name="id_usuario" value="{{ $usuario->id }}">
+                                                <button type="submit" class="btn btn-danger">Eliminar</button>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
                             <!-- Editar Usuario -->
-                            <form action="{{ route('editarUsuario') }}" method="post">
+                            <form action="{{ route('editarUsuario') }}" method="post" style="display:inline;">
                                 @csrf
                                 <input type="hidden" name="id_usuario" value="{{ $usuario->id }}">
                                 <button type="submit" class="btn btn-warning">Editar Usuario</button>
                             </form>
+
                             <!-- Ver mensajes -->
-                            <form action="{{ route('mostrarComentarios') }}" method="post">
+                            <form action="{{ route('mostrarComentarios') }}" method="post" style="display:inline;">
                                 @csrf
                                 <input type="hidden" name="id_usuario" value="{{ $usuario->id }}">
                                 <button type="submit" class="btn btn-info">Ver mensajes</button>
                             </form>
-
                         </td>
                     </tr>
                 @endforeach
