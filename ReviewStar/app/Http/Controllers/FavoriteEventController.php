ea8268def9a45969aaf8beb124dcdd9cb7058123
koke <Koke@DESKTOP-NUCPRM4.(none)>
2025-03-07 18:56:41 +0100

    favoritos correctamente funcionando, la vista no carga correctamente pero no da ningun error

--- ReviewStar/app/Http/Controllers/FavoriteEventController.php
+++ ReviewStar/app/Http/Controllers/FavoriteEventController.php
@@ -7,7 +7,6 @@ use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Cookie;
 
 class FavoriteEventController extends BaseController
 {
